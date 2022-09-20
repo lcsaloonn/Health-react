d431858ee8a9e71ff0d54278923674a99533fc65
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-20 17:19:34 +0200

    remove comment

--- health-react/src/components/navBar/navBar.tsx
+++ health-react/src/components/navBar/navBar.tsx
@@ -3,27 +3,6 @@ import "./navBar.scss";
 const NavBar = () => {
   return (
     <>
-      {/* <nav role="navigation" className="navbar">
-        <div id="menuToggle">
-          <input type="checkbox" id="checkbox" />
-          <span></span>
-          <span></span>
-          <span></span>
-          <ul id="menu">
-            <li>
-              <Link to="/" onClick={closePanel}>
-                Menu
-              </Link>
-              <Link to="#" onClick={closePanel}>
-                Profile
-              </Link>
-              <Link to="#" onClick={closePanel}>
-              session
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav> */}
       <div className="navbar">
         <input id="toggle" type="checkbox" />
 
