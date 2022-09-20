17aadb67a59b244c118c1655955699afb2706fa6
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-20 19:05:11 +0200

    creation component logo

---
+++ health-react/src/components/logo/logo.tsx
@@ -0,0 +1,11 @@
+import "./logo.scss";
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span className="logo-1">The</span>
+      <span className="logo-2">Healthy</span>
+      <span className="logo-3">Project</span>
+    </div>
+  );
+};
+export default Logo;
