92e91f16760487901f2bdb7f9d4df8d319883f9a
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-20 14:53:11 +0200

    added routes

--- health-react/src/App.tsx
+++ health-react/src/App.tsx
@@ -1,34 +1,18 @@
 import React from "react";
-import { BasicButton } from "./components/buttons";
-import { BasicForm } from "./components/forms/index";
-import { GalleryCategory } from "./components/gallery";
-import { BasicFormMock } from "./Mocks/BasicFormMocks";
-import { ButtonStyleEnum } from "./utils/types/enums/buttonStyleEnum";
-import { IFormInput } from "./utils/types/IFormInput";
+import { Link, Routes, Route } from "react-router-dom";
+import NavBar from "./components/navBar/navBar";
 import MainView from "./views/main-view/mainView";
 
 function App() {
-  const input: IFormInput[] = BasicFormMock;
-
   return (
-    <div className="app">
-      {/* <BasicForm inputs={input}></BasicForm>
-      <BasicButton
-        onClick={() => {
-          console.log("click");
-        }}
-        children="Test"
-      ></BasicButton>
-
-      <BasicButton
-        onClick={() => {
-          console.log("click");
-        }}
-        children="TEST"
-        buttonStyle={ButtonStyleEnum.SECONDARY}
-      ></BasicButton> */}
-      <MainView></MainView>
-    </div>
+    <>
+      <NavBar />
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<MainView />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
