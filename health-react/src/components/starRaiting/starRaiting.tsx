c9ae98fc09398b87038d0d6c60b6c42ef2c7e03c
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-22 08:55:32 +0200

    creation starRaiting component

---
+++ health-react/src/components/starRaiting/starRaiting.tsx
@@ -0,0 +1,32 @@
+import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
+import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { StarService } from "../../services/starsService/star.service";
+import "./starRaiting.scss";
+
+const StarRaitingCompponent = ({ num }: { num: number }) => {
+  const starService = new StarService().createStart(num);
+
+  let fullStars: JSX.Element[] = [];
+  let halfstar =
+    starService.halfStars == 1 ? (
+      <FontAwesomeIcon icon={faStarHalf} className="icon star " />
+    ) : null;
+
+  for (let i = 1; i <= starService.fullStars; i++) {
+    fullStars.push(<FontAwesomeIcon icon={faStar} className="icon star " />);
+  }
+  let emptyStars: JSX.Element[] = [];
+  for (let i = 1; i <= starService.emptyStars; i++) {
+    emptyStars.push(<FontAwesomeIcon icon={emptyStar} className="icon star" />);
+  }
+
+  return (
+    <div className="stars-raiting">
+      <span>{fullStars}</span>
+      <span>{halfstar}</span>
+      <span> {emptyStars}</span>
+    </div>
+  );
+};
+export default StarRaitingCompponent;
---
+++ health-react/src/services/starsService/star.service.tsx
@@ -0,0 +1,25 @@
+export class StarService {
+  numberstarMax = 5;
+
+  createStart(num: number) {
+    if (!this.verificationNumber(num))
+      throw new Error("the num needs to be below 5");
+    else {
+      return this.numberOfStars(num);
+    }
+  }
+
+  private verificationNumber(num: number): boolean {
+    if (num > this.numberstarMax) return false;
+    else return true;
+    // si 4.8 non mais 4.5 oui
+  }
+
+  private numberOfStars(num: number) {
+    const fullStars = Math.floor(num);
+    const halfStars = fullStars - num === 0 ? 0 : 1;
+    const emptyStars =
+      fullStars + halfStars < 5 ? 5 - (fullStars + halfStars) : 0;
+    return { halfStars, fullStars, emptyStars };
+  }
+}
