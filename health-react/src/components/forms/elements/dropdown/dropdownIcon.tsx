3509016c432fbf3375b5a847f3222b814fd6d2cd
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-22 14:14:15 +0200

    refactor css

--- health-react/src/components/forms/elements/dropdown/dropdownIcon.tsx
+++ health-react/src/components/forms/elements/dropdown/dropdownIcon.tsx
@@ -28,20 +28,18 @@ const DropDownListIcon = ({
   };
 
   return (
-    <div className="t">
-      <div className="dropdown" onClick={() => dropdown()}>
-        <input type="text" className="textbox" placeholder={title} readOnly />
-        <div className="option">
-          {data.map((data: Dropdown, id: number) => (
-            <div
-              onClick={() => show(data.optionValue)}
-              id={data.optionValue}
-              key={id}
-            >
-              {checkIcon} {data.optionText}
-            </div>
-          ))}
-        </div>
+    <div className="dropdown" onClick={() => dropdown()}>
+      <input type="text" className="textbox" placeholder={title} readOnly />
+      <div className="option">
+        {data.map((data: Dropdown, id: number) => (
+          <div
+            onClick={() => show(data.optionValue)}
+            id={data.optionValue}
+            key={id}
+          >
+            {checkIcon} {data.optionText}
+          </div>
+        ))}
       </div>
     </div>
   );
