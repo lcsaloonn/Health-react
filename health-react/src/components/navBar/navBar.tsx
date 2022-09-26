6789dee306e4158362e1335ec94f0e4aa6e82595
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-26 11:50:03 +0200

    navbar correction

--- health-react/src/components/navBar/navBar.tsx
+++ health-react/src/components/navBar/navBar.tsx
@@ -3,28 +3,26 @@ import "./navBar.scss";
 const NavBar = () => {
   return (
     <>
-      <div className="navbar">
-        <input id="toggle" type="checkbox" />
+      <input id="toggle" type="checkbox" />
 
-        <label className="toggle-container" htmlFor="toggle">
-          <span className="button button-toggle"></span>
-        </label>
+      <label className="toggle-container" htmlFor="toggle">
+        <span className="button button-toggle"></span>
+      </label>
 
-        <nav className="nav">
-          <Link className="nav-item" to="/">
-            Menu
-          </Link>
-          <Link className="nav-item" to="register">
-            Profil
-          </Link>
-          <Link className="nav-item" to="#">
-            Sessions
-          </Link>
-          <Link className="nav-item" to="exercices">
-            Exerices
-          </Link>
-        </nav>
-      </div>
+      <nav className="nav">
+        <Link className="nav-item" to="/">
+          Menu
+        </Link>
+        <Link className="nav-item" to="register">
+          Profil
+        </Link>
+        <Link className="nav-item" to="#">
+          Sessions
+        </Link>
+        <Link className="nav-item" to="exercices">
+          Exerices
+        </Link>
+      </nav>
     </>
   );
 };
