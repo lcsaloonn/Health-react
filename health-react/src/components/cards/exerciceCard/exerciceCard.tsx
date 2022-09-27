8a3ece4bdf7649331b2df9e69dd9bed1d4f751ef
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-09-27 12:28:22 +0200

    added props type of api

--- health-react/src/components/cards/exerciceCard/exerciceCard.tsx
+++ health-react/src/components/cards/exerciceCard/exerciceCard.tsx
@@ -1,13 +1,13 @@
-import { IExerciceCard } from "utils/types/IExerciceCard";
+import { IExercice } from "utils/types/IExercice";
 import { truncateText } from "utils/util/text-format/textFormat";
 import StarRaitingCompponent from "../../starRaiting/starRaiting";
 import "./exerciceCard.scss";
 
-const ExerciceCard = ({ props }: { props: IExerciceCard }) => {
+const ExerciceCard = ({ props }: { props: IExercice }) => {
   return (
     <div className="exercice-card">
       <div className="thumbnail">
-        <img src={`assets/img/${props.link.url}`} alt={props.link.alt} />
+        <img src={`assets/img/${props.imageUrl}`} alt="#" />
       </div>
       <div className="title">{props.title}</div>
       <div className="text">{truncateText(props.description, 200)}</div>
