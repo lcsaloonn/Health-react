13d7b287b176020e3a969adf3acca5c91761f6b4
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-10-13 10:11:59 +0200

    added display of errors

--- health-react/src/views/Login/login.tsx
+++ health-react/src/views/Login/login.tsx
@@ -1,6 +1,17 @@
+import { useRef, useState } from "react";
 import "./login.scss";
 
 export function LoginView() {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const [isError, setIsError] = useState(true);
+
+  function verifyData() {
+    if (usernameRef.current?.value !== "" && passwordRef.current?.value !== "")
+      setIsError(false);
+    else setIsError(true);
+  }
+
   return (
     <div className="login-form main-container grid gap-6 grid-cols-4 md:grid-cols-7 lg:grid-cols-9">
       <div className="login-form-title col-span-4 md:col-start-3 md:col-end-6 lg:col-start-4 lg:col-end-7">
@@ -8,15 +19,28 @@ export function LoginView() {
       </div>
       <div className="login-form-input col-span-4 md:col-start-3 md:col-end-6 lg:col-start-4 lg:col-end-7">
         <label htmlFor="username">Nom d'utilisateur</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" ref={usernameRef} />
       </div>
 
       <div className="login-form-input col-span-4 md:col-start-3 md:col-end-6 lg:col-start-4 lg:col-end-7">
         <label htmlFor="password">Mot de passe</label>
-        <input type="password" id="password" />
+        <input type="password" id="password" ref={passwordRef} />
       </div>
 
-      <button className="login-form-btn col-span-4 md:col-start-3 md:col-end-6 lg:col-start-4 lg:col-end-7">
+      <div
+        className={
+          isError
+            ? "login-form-error col-span-4 md:col-start-3 md:col-end-6 lg:col-start-4 lg:col-end-7"
+            : "hidden"
+        }
+      >
+        Il faut remplir tous les champs
+      </div>
+
+      <button
+        className="login-form-btn col-span-4 md:col-start-3 md:col-end-6 lg:col-start-4 lg:col-end-7"
+        onClick={() => verifyData()}
+      >
         Valider
       </button>
     </div>
