2151347e45361f5eabeccb4b6e8bd663b6b705eb
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-10-13 17:08:44 +0200

    removed registerform

--- health-react/src/components/forms/index.tsx
+++ health-react/src/components/forms/index.tsx
@@ -1,5 +1,4 @@
 import BasicForm from "./basicForm/basicForm";
 import { DropDownListIcon } from "./elements";
-import RegisterForm from "../../views/registerForm/registerForm";
 
-export { BasicForm, DropDownListIcon, RegisterForm };
+export { BasicForm, DropDownListIcon };
