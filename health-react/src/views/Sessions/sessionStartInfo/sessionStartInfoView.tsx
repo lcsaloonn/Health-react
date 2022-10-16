6fda761773879058e3f156853b790a0d31c16d39
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-10-16 15:01:15 +0200

    creation session start info template

--- health-react/src/views/Sessions/sessionStartInfo/sessionStartInfoView.tsx
+++ health-react/src/views/Sessions/sessionStartInfo/sessionStartInfoView.tsx
@@ -1,3 +1,46 @@
+import { Link } from "react-router-dom";
+
 export function SessionStartInfoView() {
-  return <></>;
+  return (
+    <div className="session-info">
+      <div className="session-info-title"> Session test</div>
+      <div className="session-info-bodyPart">
+        <div className="session-info-bodyPart-title">
+          Cette session vous permet de développé
+        </div>
+        <div className="session-info-bodyPart-list">
+          <span className="session-info-bodyPart-list-item">bras</span>
+          <span className="session-info-bodyPart-list-item">dos</span>
+          <span className="session-info-bodyPart-list-item">pecs</span>
+          <span className="session-info-bodyPart-list-item">jambre</span>
+        </div>
+      </div>
+      <div className="session-info-parts">
+        <div className="session-info-parts-ex1">
+          <div className="session-info-parts-ex1-img">
+            <img src="" alt="" />
+          </div>
+          <div className="session-info-parts-ex1-container">
+            <div className="session-info-parts-ex1-container-title">ex1</div>
+            <p className="session-info-parts-ex1-container-description">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga rem
+              porro neque iure libero culpa maiores veritatis, ducimus tenetur a
+              at nulla corporis explicabo accusantium facilis perspiciatis?
+              Totam, suscipit quisquam.
+            </p>
+            <Link to="\">en savoir plus</Link>
+            <div className="session-info-parts-ex1-container-series">
+              Nombre de série: <b>2</b>
+            </div>
+            <div className="session-info-parts-ex1-container repetition">
+              Nombre de répétition: <b>4</b>
+            </div>
+            <div className="session-info-parts-ex1-container-weight">
+              Poids: variable
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
