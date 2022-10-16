7853f853cf416683f91423227cbf6d2417dbae11
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-10-16 10:28:14 +0200

    create program view

--- health-react/src/views/ProgramList-view/programListView.tsx
+++ health-react/src/views/ProgramList-view/programListView.tsx
@@ -1,3 +1,22 @@
+import { ProgramCardComponent } from "components/cards/programCard/programCard.component";
+import "./programListView.scss";
+
 export function ProgramListView() {
-  return <></>;
+  return (
+    <div className="main-container programs">
+      <div className="programs-title">Choisis ton Programme</div>
+      <div className="programs-title">Et commence ton entrainement</div>
+      <div className="programs-cards">
+        <div>
+          <ProgramCardComponent />;
+        </div>
+        <div>
+          <ProgramCardComponent />;
+        </div>
+        <div>
+          <ProgramCardComponent />;
+        </div>
+      </div>
+    </div>
+  );
 }
